64fe29f571aaf293bb32f2865212c8a2e31df5b1
MasterH6168 <MasterH6168@gmail.com>
2021-01-17 15:42:56 -0600

    added practice teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice/PracticeTeleOp.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.Practice;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Practice")
+public class PracticeTeleOp extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
